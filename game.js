aba4338276f1845c18f61c9962e4199e95d160dc
Sergiu <szatmari.sergiu@gmail.com>
2020-10-11 10:55:57 +0300

    High-score added

--- game.js
+++ game.js
@@ -2,30 +2,59 @@ const $ = (selector) => { return document.querySelector(selector); };
 
 const Keys = { LEFT: 37, UP: 38, RIGHT: 39, DOWN: 40 }, 
     player = { x: 10, y: 10 }, 
-    apple = { x: 15, y: 15 }, 
-    velocity = { x: 0, y: 0 },
-    trail = [];
+    trail = [],
+    velocity = { x: 0, y: 0, set: (newX, newY) => { velocity.x = newX; velocity.y = newY; } },
+    apple = { x: 15, y: 15, spawn: () => { apple.x = apple.y = Math.floor(Math.random() * tileCount); } }, 
+    highscore = { score: 0, player: null, get: () => { const hs = getHighscore(); if (hs) { highscore.score = hs.score; highscore.player = hs.player; } } };
 
-let context, canvas,
+let context, canvas, 
     lastKey = 0,
     gridSize = 20, 
     tileCount = 20,
     tail = 5,
     score = 0,
-    scoreElem = $('#score'); 
+
+    scoreElem = $('#score'),
+    highScoreElem = $('#high-score'),
+    hsScoreElem = $('#hs'),
+    hsPlayerElem = $('#name'); 
 
 window.onload = () => {
     canvas = $('#gc');
     context = canvas.getContext('2d');
+
     document.addEventListener('keydown', onKeyPressed);
-    setInterval(game, 1000/15);
+    $('#reset').addEventListener('click', resetHighScore);
+
+    setTimeout(game, 1000/15);
     setTimeout(clearInstructions, 5000);
+    
+    highscore.get();
+    if (!highscore.player) { highScoreElem.classList.add('invisible'); }
+    else setHighscore(highscore.score, highscore.player);
 }
 
 const clearInstructions = () => {
     const elem = $('.how-to');
     elem.classList.add('hidden');
     setTimeout(() => { elem.remove(); }, 1000);
+}
+
+const resetHighScore = () => {
+    localStorage.removeItem('snakeHighScore');
+    highScoreElem.classList.add('invisible');
+}
+
+const setHighscore = (score, player) => {
+    localStorage.setItem('snakeHighScore', JSON.stringify({ score, player }));
+    hsScoreElem.innerHTML = highscore.score; 
+    hsPlayerElem.innerHTML = highscore.player;
+    highScoreElem.classList.remove('invisible');
+}
+
+const getHighscore = () => {
+    const json = localStorage.getItem('snakeHighScore');
+    return !json ? null : JSON.parse(json);
 }
 
 const isInvalid = (keyCode) => {
@@ -48,20 +77,16 @@ const onKeyPressed = (event) => {
     lastKey = keyCode;
     switch (keyCode) {
         case Keys.LEFT:
-            velocity.x = -1;
-            velocity.y = 0;
+            velocity.set(-1, 0);
             break;
         case Keys.UP:
-            velocity.x = 0;
-            velocity.y = -1;
+            velocity.set(0, -1);
             break;
         case Keys.RIGHT:
-            velocity.x = 1;
-            velocity.y = 0;
+            velocity.set(1, 0);
             break;
         case Keys.DOWN:
-            velocity.x = 0;
-            velocity.y = 1;
+            velocity.set(0, 1);
             break;
     }
 }
@@ -72,6 +97,8 @@ const setScore = (scoreNr) => {
     scoreElem.innerHTML = score;
     console.log(`Current score: ${ score }`);
 }
+
+const nonUnique = a => new Set(a).size !== a.length;
                 
 const game = () => {
     player.x += velocity.x;
@@ -88,6 +115,12 @@ const game = () => {
     for (let i = 0; i < trail.length; i++) {
         context.fillRect(trail[i].x * gridSize, trail[i].y * gridSize, gridSize - 2, gridSize - 2);
         if (trail[i].x == player.x && trail[i].y == player.y) {
+            if (!nonUnique(trail) && trail.length > 2 && (score > highscore.score)) {
+                console.log({ trail });
+                const name = prompt('New high score! Enter your name');
+                setHighscore(score, name);
+                location.reload();
+            }
             setScore(0);
         }
     }
@@ -97,10 +130,10 @@ const game = () => {
 
     if (apple.x == player.x && apple.y == player.y) {
         setScore(++score);
-        apple.x = Math.floor(Math.random() * tileCount);
-        apple.y = Math.floor(Math.random() * tileCount);
+        apple.spawn();
     }
 
     context.fillStyle = '#86b486';
     context.fillRect(apple.x * gridSize, apple.y * gridSize, gridSize - 2, gridSize - 2);
+    setTimeout(game, 1000/15);
 }
